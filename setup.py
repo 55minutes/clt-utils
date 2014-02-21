3e7d3a8fbfd6ad4307fc6a3110fc849952e8ba24
George Song <george@55minutes.com>
2014-02-21 14:10:48 -0800

    Don't require distribute

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
 PROJECT_URL = 'https://github.com/55minutes/clt-utils'
 DOWNLOAD_URL = '{0}/archive/master.tar.gz'.format(PROJECT_URL)
 
-REQUIRES = ['ansicolors', 'distribute']
+REQUIRES = ['ansicolors']
 if sys.version_info < (2, 7):
     REQUIRES.append('argparse')
 
